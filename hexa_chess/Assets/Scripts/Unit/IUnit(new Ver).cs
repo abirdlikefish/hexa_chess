aba715d9e4a6aff39a7973bd9660ff06396fa1f3
priceEXE <152944571+priceEXE@users.noreply.github.com>
2025-03-11 17:54:59 +0800

    UnitManager
    目前版本：Manager与Mono解绑

--- hexa_chess/Assets/Scripts/Unit/IUnit(new Ver).cs
+++ hexa_chess/Assets/Scripts/Unit/IUnit(new Ver).cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public interface IUnit  
 {
-    void Attack(IUnit unit);
-    void GetDamage(int danage);
+    void Move(int cost);//参数待定
 
-    void Move();
+    void Attack(IUnit other);//传入攻击对象的脚本接口
 
+    void Station();//驻扎
+
+    void Rest();//休息
+
+    void Dismiss();//解散
+
+    void Skip();//跳过
+
+    void GetDamage(int damage);
+
+    UnitStates GetStates();
 
 }
--- hexa_chess/Assets/Scripts/Unit/Unit(new Ver).cs
+++ hexa_chess/Assets/Scripts/Unit/Unit(new Ver).cs
@@ -8,27 +8,123 @@ public class Unit_new : MonoBehaviour, IUnit
     public UnitConfig unitConfig;
     //单位当前状态
     public  UnitStates unitState;
+    //操作增益
+    public OprationBuff oprationBuff;
     //生命值
     public int currentHp;
+
+    public int currentAction;//当前行动力
+
+
 
     public void UnitInitialize(UnitConfig iniConfig)
     {
         unitConfig = iniConfig;
-        unitState = UnitStates.Waiting;
+        unitState = UnitStates.Able;
     }
-    public void Attack(IUnit unit)
+    public void Attack(IUnit other)
     {
-        unit.GetDamage(unitConfig.Attak);
+        if(unitState == UnitStates.Able)
+        {
+            //消耗当前所有行动力
+            ActionCheck(currentAction);
+            Debug.Log("单位攻击指令执行！");
+            other.GetDamage(unitConfig.Attak);
+        }
+        else
+        {
+            Debug.Log("单位不可操作！");
+        }
     }
 
     public void GetDamage(int damage)
     {
-        currentHp -= damage;
+        int finalDamage;
+        switch (oprationBuff)
+        {
+            case OprationBuff.Rest: finalDamage = damage += 1;
+            break;
+            default:    finalDamage = damage;
+            break;
+        }
+        currentHp -= finalDamage;
+        DestroyCheck();
     }
 
-    public void Move()
+    public void Move(int cost)
     {
-        
+        if(unitState == UnitStates.Able)
+        {
+            Debug.Log("移动！");
+            //todo:棋子寻路和移动方式
+            ActionCheck(cost);
+        }
+        else
+        {
+            Debug.Log("单位不可操作！");
+        }
     }
 
+    public void Station()
+    {
+
+    }
+
+    public void Rest()
+    {
+        oprationBuff = OprationBuff.Rest;
+    }
+
+    public void Dismiss()
+    {
+        ReturnCost();
+        RecycleUnit();
+    }
+
+    public void Skip()
+    {
+        ActionCheck(currentAction);
+    }
+    //行动力检定，判断单位操作是否转入无法操作
+    private void ActionCheck(int ActionCost)
+    {
+        if(unitState == UnitStates.Able && currentAction != 0)   
+        {
+            currentAction -= ActionCost;
+            if(currentAction < 0)   currentAction = 0;
+        } 
+        if(currentAction == 0)  unitState = UnitStates.Disable;
+    }
+    //回合结束检定，将单位转入可操作
+    private void RoundEndCheck()
+    {
+        unitState = UnitStates.Able;
+        currentAction = unitConfig.Action;
+    }
+    //摧毁检定
+    private void DestroyCheck()
+    {
+        if(currentHp <= 0)
+        {
+            Debug.Log("单位被摧毁");
+        }
+    }
+
+    //资源返还函数，如何返还存疑
+    private void ReturnCost()
+    {
+        Debug.Log("返还资源！");
+    }
+
+    private void RecycleUnit()
+    {
+        Debug.Log("回收单位！");
+        Destroy(gameObject);
+        //todo:回收对象池
+    }
+
+    public UnitStates GetStates()
+    {
+        return unitState;
+    }
 }
