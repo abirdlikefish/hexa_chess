cbe0597df3ac8fe7cf1723d31a973c15ba5acb07
priceEXE <152944571+priceEXE@users.noreply.github.com>
2025-03-12 16:09:57 +0800

    移除旧版本Unit

--- hexa_chess/Assets/Scripts/Unit/Infantry.cs
+++ hexa_chess/Assets/Scripts/Unit/Infantry.cs
@@ -4,17 +4,5 @@ using UnityEngine;
 
 public class Infantry : Unit
 {
-    private void Start()
-    {
-        InitInformation(Enum.UnitType.Infantry,5,3,2,1,5,1,false);
-        GameManager.instance.AddUnitIntoPlayerUnits(this);
-    }
-
-    private void Update()
-    {
-        if (this.currentState.state == Enum.UnitStates.Action)
-        {
-            
-        }
-    }
+    
 }
