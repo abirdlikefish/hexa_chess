414d20c9ca6a8cea5620395577b74d5a47cd4363
龙门特产龙井茶 <tieyewang054@gmail.com>
2025-03-16 20:18:39 +0800

    改了个名

---
+++ hexa_chess/Assets/Scripts/Game/PlayerState/PlayerRound_SelectedBase.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerRound_SelectedBase : PlayerRoundState
+{
+    public PlayerRound_SelectedBase(PlayerStateMachine _playerStateMachine, MyEnum.PlayerRoundState _playerState) : base(_playerStateMachine, _playerState)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    private void CreateUnit()
+    {
+        
+    }
+}
