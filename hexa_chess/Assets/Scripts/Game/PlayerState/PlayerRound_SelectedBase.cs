79e3df432816eb20aecc15d7dbddf44312301076
龙门特产龙井茶 <tieyewang054@gmail.com>
2025-03-19 16:37:34 +0800

    重改了CreateUnit方法

--- hexa_chess/Assets/Scripts/Game/PlayerState/PlayerRound_SelectedBase.cs
+++ hexa_chess/Assets/Scripts/Game/PlayerState/PlayerRound_SelectedBase.cs
@@ -1,10 +1,14 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerRound_SelectedBase : PlayerRoundState
 {
     public PlayerRound_SelectedBase(PlayerStateMachine _playerStateMachine, MyEnum.PlayerRoundState _playerState) : base(_playerStateMachine, _playerState)
     {
     }
+
+    public MyEnum.ArmyType selectedType;
 
     public override void Enter()
     {
@@ -20,13 +24,26 @@ public class PlayerRound_SelectedBase : PlayerRoundState
         MyEvent.OnClick_skipBtn -= PressSkip;
     }
 
-    private void CreateUnit()
+    private void CreateUnit(MyEnum.ArmyType armyType)
     {
-        //todo:创造Unit
+        Vector2Int? createGrid = playerStateMachine.selectedGrid;
+        MyEnum.MoveDirection randomDir= (MyEnum.MoveDirection)Random.Range(0, 6);
+        createGrid = createGrid ?? createGrid.Value + MyConst.MoveStep[randomDir];
+        UnitManager.Instance.CreateNewUnit(MyEnum.TheOperator.Player,createGrid.Value, armyType);
     }
 
     private void PressSkip()
     {
-        
+        IUnit ableUnit = UnitManager.Instance.GetAbleUnit(MyEnum.TheOperator.Player);
+        if (ableUnit == null)//如果没有可以动的单元了 就换边
+        {
+            GameStateMachine.Instance.ChangeState(MyEnum.GameState.EnemyRound);
+        }
+        else
+        {
+            playerStateMachine.selectedUnit = ableUnit;
+            playerStateMachine.selectedGrid = ableUnit.Coord;
+            playerStateMachine.ChangeState(MyEnum.PlayerRoundState.WaitInput_WhichAction);
+        }
     }
 }
