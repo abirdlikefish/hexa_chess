887447c1d48596f4723ea5db41a4a349908855b0
abirdlikefish <abirdlikefish@qq.com>
2025-03-20 23:12:48 +0800

    修bug

--- hexa_chess/Assets/Scripts/Map/MapManager.cs
+++ hexa_chess/Assets/Scripts/Map/MapManager.cs
@@ -529,7 +529,7 @@ public class MapManager : IMapManager , IMapManager_edit
     {
         if(!IsInMap(endCoord) || gridMap[endCoord.x, endCoord.y].currentUIState != MyEnum.GridUIState.Legal)
         {
-            Debug.LogError($"GetMovePath: Out of Map or Illegal {IsInMap(endCoord)} {gridMap[endCoord.x, endCoord.y].currentUIState}");
+            // Debug.LogError($"GetMovePath: Out of Map or Illegal {IsInMap(endCoord)} {gridMap[endCoord.x, endCoord.y].currentUIState}");
             moveCost = -1;
             return null;
         }
